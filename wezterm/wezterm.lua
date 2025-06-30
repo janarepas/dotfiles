6f691914f3bd789769297ddb68662443b4e3c5ab
janarepas <jana.repas@gmail.com>
2025-06-30 07:15:45 +0200

    config tweaks

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,18 +2,18 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
--- Átlátszóság
+-- General
+config.initial_cols = 100
+config.initial_rows = 60
+config.font_size = 18
+config.color_scheme = "Tokyo Night"
+
+-- Transparency
 config.window_background_opacity = 0.5
 config.macos_window_background_blur = 45
 config.inactive_pane_hsb = {
   saturation = 1.0,
   brightness = 0.4,
 }
-
--- Alapbeállítások
-config.initial_cols = 100
-config.initial_rows = 60
-config.font_size = 18
-config.color_scheme = "Tokyo Night"
 
 return config
